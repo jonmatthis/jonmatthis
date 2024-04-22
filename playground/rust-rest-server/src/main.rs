0ffcb7ad0b70c82ddb955eb1631d568cfcb9802d
Jonathan Samir Matthis <jonmatthis@gmail.com>
2024-04-22 11:34:52 +0000

    Apr 22, 2024, 7:34 AM

--- playground/rust-rest-server/src/main.rs
+++ playground/rust-rest-server/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web ,App, HttpResponse, HttpServer, Responder};
+use::serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct User {
+    name: String,
+    age: u8,
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/users")]
+async fn get_users() -> impl Responder {
+    let users = vec![
+        User {
+            name: "John".to_string(),
+            age: 20,
+        },
+        User {
+            name: "Jane".to_string(),
+            age: 21,
+            },
+    ];
+
+    web::Json(users)
 }
