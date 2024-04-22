4afeb09bbc607cdbd58fbb06494ea4fe0c172f8e
Jonathan Samir Matthis <jonmatthis@gmail.com>
2024-04-22 16:01:57 +0000

    Apr 22, 2024, 12:01 PM

--- playground/rust_webcam_poc/src/main.rs
+++ playground/rust_webcam_poc/src/main.rs
@@ -1,19 +1,25 @@
 extern crate opencv;
 use opencv::{
     prelude::*,
-    highgui,
     videoio,
+    highgui,
     core,
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut cap = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
     if !videoio::VideoCapture::is_opened(&cap)? {
-        panic!("Unable to open ccamamaerea")
+        panic!("Unable to open camera");
     }
 
-let window_name = "Camamera";
-highgui::named_window(window_name, highgui::WINDOW_AUTOSIZE)?;
+    // Set the frame width and height to 1920x1080
+    videoio::VideoCapture::set(&mut cap, videoio::CAP_PROP_FRAME_WIDTH, 1920.0)?;
+    videoio::VideoCapture::set(&mut cap, videoio::CAP_PROP_FRAME_HEIGHT, 1080.0)?;
+
+    // Optionally, set the frame rate (depending on camera and driver support)
+    // videoio::VideoCapture::set(&mut cap, videoio::CAP_PROP_FPS, 30.0)?;
+    let window_name = "Camera";
+    highgui::named_window(window_name, highgui::WINDOW_AUTOSIZE)?;
 
 loop {
     let mut frame = Mat::default();
@@ -24,13 +30,14 @@ loop {
 
     if frame.size()?.width > 0 {
         println!("Captured an image of size {}x{}", frame.size()?.width, frame.size()?.height);
-        highgui::imshow("Camamera", &frame)?;
+            highgui::imshow(window_name, &frame)?;
         if highgui::wait_key(10)? > 0 {
             break;
             }
     } else {
-        println!("Unable to capture an image");
+            println!("No image captured, frame is empty.");
     }
 }
+
 Ok(())
 }
